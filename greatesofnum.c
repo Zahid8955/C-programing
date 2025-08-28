64c57ddddde221b984c2288498a06ca71454f23b
Mohammad Zahid <153949585+Zahid8955@users.noreply.github.com>
2025-08-28 14:32:59 +0530

    Add files via upload

---
+++ greatesofnum.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int num1 , num2;
+    printf("please! Enter the number1.");
+    scanf("%d",&num1);
+    printf("please Enter the number2.");
+    scanf("%d",&num2);
+    if ("num1>num2")
+    printf("%d is greater then %d",num1,num2);
+    else
+    printf("%d is greater then %d",num2,num1);
+    return 0;
+}
